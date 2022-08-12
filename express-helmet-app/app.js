a3ea8b9e16e3e4d4abc4e19917d9aaaff7634975
xu chen <xuchen@xus-MacBook-Pro.attlocal.net>
2022-08-11 18:48:29 -0700

    inclass

---
+++ express-helmet-app/app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express');
+const helmet = require('helmet');
+const app = express();
+
+// app.disable('etag');
+
+app.use(helmet());
+
+app.get('/', (req, res) => {
+  console.log('route \'/\' called!');
+  res.send({ res: 'Sending 200 as response' });
+});
+
+app.listen(3000, () => {
+  console.log('Express app is up and running on port 3000!');
+});
